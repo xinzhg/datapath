af42381ab42c1ba85b63c5d1e5ad35fedb981d07
luminaire1202@gmail.com <luminaire1202@gmail.com@4386ba03-4729-655b-538b-0d775d94e7a0>
2013-01-08 17:46:25 +0000

    Removed several unneeded directories.

--- trunk/src/Tool_Planner/executable/jointest.sql
+++
@@ -1,2 +0,0 @@
-SELECT SUM(l_extendedprice * (1 - l_discount)) AS revenue FROM lineitem, orders WHERE l_orderkey = o_orderkey AND o_orderkey < 2000000000 AND l_orderkey < 2000000000;
-;
--- trunk/src/Tool_Planner/executable/q2.sql
+++
@@ -1,8 +0,0 @@
-SELECT AVG(o_totalprice) AS agg
-FROM orders, customer, nation
-WHERE
-o_custkey = c_custkey
-AND o_orderdate > '1997-03-02'
-AND o_orderdate < '1997-05-09'
-AND n_nationkey = c_nationkey 
-AND n_name = 'FRANCE' OR n_name = 'GERMANY';;
