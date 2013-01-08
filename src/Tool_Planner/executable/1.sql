e3ab51edc5c2b2de7587a4602e85d8e70f2fad08
Luminaire1202 <luminaire1202@gmail.com>
2013-01-08 17:46:25 +0000

    Removed several unneeded directories.

--- src/Tool_Planner/executable/1.sql
+++
@@ -1,7 +0,0 @@
-SELECT SUM(l_extendedprice * (1 - l_discount)) AS revenue
-FROM lineitem, orders
-WHERE
-l_orderkey = o_orderkey
-AND o_orderdate >= DATE(1997,2,1)
-AND o_orderdate < DATE(1997,5,7);
-;
